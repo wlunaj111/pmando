b8f293095c8f6bde2fe17554371374278dc5595a
wluna <wlunaj111@gmail.com>
2022-12-30 22:05:35 -0500

    add dark mode angular material angular 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pmando';
+  @HostBinding('class')componentCssClass: any;
+
+  constructor(public overlayContainer: OverlayContainer){}
+  public onSetTheme(e:string){
+    this.overlayContainer.getContainerElement().classList.add(e);
+    this.componentCssClass = e;
+  }
+
 }
